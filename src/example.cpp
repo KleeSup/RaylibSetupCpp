48620050b319e91236b5f8b8752ee28994d5544e
KleeSup <bernd_brot.1@web.de>
2026-01-10 23:43:40 +0100

    Minor renaming changes/fixes

--- src/example.cpp
+++ src/example.cpp
@@ -1,3 +1,16 @@
+#include "raylib.h"
+
 int main(){
-  return 0;
+    // -- Setup window --
+    SetConfigFlags(FLAG_VSYNC_HINT);
+    InitWindow(960, 720, "Example");
+    // -- Render loop --
+    while(!WindowShouldClose()){
+        ClearBackground(WHITE);
+        BeginDrawing();
+        DrawFPS(0,0);
+        EndDrawing();
+    }
+    // -- Cleanup --
+    CloseWindow();
 }
